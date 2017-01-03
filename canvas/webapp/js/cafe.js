4b34d23179df112038761e99f197edc6f23e72aa
刘海文 <mars@cosmo.local>
2017-01-03 22:22:25 +0800

    优化

--- canvas/webapp/js/cafe.js
+++ canvas/webapp/js/cafe.js
@@ -1,15 +1,19 @@
+
 /****************config****************/
 const ctxPadding = 10;  // 画布padding
 const ctxWidth = 1000;  // 画布去padding后宽
 const ctxHeight = 1000; // 画布去padding后高
 const strokeWidth = 5;  // 器皿壁厚
-const strokeColor = '#000'; // 器皿颜色
-const colorMix = true;  // 是否混合颜色，false为分层
+const strokeColor = '#000'; // 器皿边框颜色
+const colorMix = false;  // 是否混合颜色，false为分层
+const fillSpeed = 5; // 每10毫秒上升高度
 
-/*图形上下文*/
-const ctx = document.getElementById('canvas').getContext('2d');
-/*离屏canvas 用于制作液体图片*/
-const offCtx = document.getElementById('offCanvas').getContext('2d');
+/*liquid图形上下文*/
+const liquidCtx = document.getElementById('liquid').getContext('2d');
+/*vessel图形上下文*/
+const vesselCtx = document.getElementById('vessel').getContext('2d');
+/*pour图形上下文*/
+const pourCtx = document.getElementById('pour').getContext('2d');
 
 /*液体数组*/
 var liquids = [];
@@ -43,31 +47,103 @@ setTimeout(function () {
         color: '#999',
         height: 150
     });
-}, 2000);
+}, 1000);
 
 setTimeout(function () {
     addLiquid({
         color: '#0F0',
         height: 100
     });
-}, 4000);
+}, 1200);
 
 /*******************/
 
 /****************method****************/
 function init() {
-    ctx.canvas.width = ctxWidth + ctxPadding * 2;
-    ctx.canvas.height = ctxHeight + ctxPadding * 2;
-    ctx.translate(ctxPadding, ctxPadding);
-    ctx.strokeStyle = strokeColor;
-    ctx.lineWidth = strokeWidth;
-    ctx.lineCap = 'round';
-    ctx.lineJoin = 'miter';
+    liquidCtx.canvas.width = ctxWidth + ctxPadding * 2;
+    liquidCtx.canvas.height = ctxHeight + ctxPadding * 2;
+    liquidCtx.translate(ctxPadding, ctxPadding);
 
-    offCtx.canvas.width = ctxWidth + ctxPadding * 2;
-    offCtx.canvas.height = ctxHeight + ctxPadding * 2;
+    vesselCtx.canvas.width = ctxWidth + ctxPadding * 2;
+    vesselCtx.canvas.height = ctxHeight + ctxPadding * 2;
+    vesselCtx.translate(ctxPadding, ctxPadding);
+    vesselCtx.strokeStyle = strokeColor;
+    vesselCtx.lineWidth = strokeWidth;
+    vesselCtx.lineCap = 'round';
+    vesselCtx.lineJoin = 'miter';
 
-    setTimeout(redraw, 0);
+    pourCtx.canvas.width = ctxWidth + ctxPadding * 2;
+    pourCtx.canvas.height = ctxHeight + ctxPadding * 2;
+    pourCtx.translate(ctxPadding, ctxPadding);
+    pourCtx.lineCap = 'round';
+
+    drawVessel();
+    redraw();
+}
+
+/*action*/
+function addLiquid(liquid) {
+    const height = liquid.height;
+    liquid.height = 0;
+    liquids.push(liquid);
+    function animate() {
+        if (height > liquid.height) {
+            liquid.height += fillSpeed;
+            redraw();
+            setTimeout(function () {
+                animate();
+            }, 10);
+        }
+    }
+
+    animate();
+}
+
+/*draw*/
+var shouldRedraw = false;
+function redraw() {
+    if (shouldRedraw == false) {
+        shouldRedraw = true;
+        setTimeout(function () {
+            clearLiquid();
+            if (colorMix) {
+                drawLiquid(mixedColor(), topHeightForLiquid(liquids.length - 1));
+            } else {
+                for (var i = liquids.length - 1; i >= 0; i--) {
+                    drawLiquid(liquids[i].color, topHeightForLiquid(i), liquids[i].height);
+                }
+            }
+            clipLiquid();
+            shouldRedraw = false;
+        }, 10);
+    }
+}
+
+function drawVessel() {
+    vesselCtx.save();
+    createVesselPath(vesselCtx);
+    vesselCtx.stroke();
+    vesselCtx.restore();
+}
+
+function clearLiquid() {
+    liquidCtx.clearRect(0, 0, ctxWidth + ctxPadding * 2, ctxHeight + ctxPadding * 2);
+}
+
+function drawLiquid(color, topHeight, height) {
+    liquidCtx.save();
+    liquidCtx.globalCompositeOperation = 'source-over';
+    liquidCtx.fillStyle = color;
+    liquidCtx.fillRect(0, ctxHeight + ctxPadding - topHeight, ctxWidth + ctxPadding * 2, height);
+    liquidCtx.restore();
+}
+
+function clipLiquid() {
+    liquidCtx.save();
+    liquidCtx.globalCompositeOperation = 'destination-in';
+    createVesselPath(liquidCtx);
+    liquidCtx.fill();
+    liquidCtx.restore();
 }
 
 /*private*/
@@ -117,70 +193,6 @@ function mixedColor() {
     if (g.length < 2) result += "0"; result += g;
     if (b.length < 2) result += "0"; result += b;
     return result;
-}
-
-/*action*/
-function addLiquid(liquid) {
-    const height = liquid.height;
-    liquid.height = 0;
-    liquids.push(liquid);
-    function animate() {
-        if (height > liquid.height) {
-            liquid.height += 8;
-            redraw();
-            setTimeout(function () {
-                animate();
-            }, 10);
-        }
-    }
-
-    animate();
-}
-
-var shouldRedraw = false;
-function redraw() {
-    if (shouldRedraw == false) {
-        shouldRedraw = true;
-        setTimeout(function () {
-            clear();
-            if (colorMix) {
-                drawLiquid(mixedColor(), topHeightForLiquid(liquids.length - 1));
-            } else {
-                for (var i = liquids.length - 1; i >= 0; i--) {
-                    drawLiquid(liquids[i].color, topHeightForLiquid(i));
-                }
-            }
-            drawVessel();
-            shouldRedraw = false;
-        }, 10);
-    }
-}
-
-function clear() {
-    ctx.clearRect(0, 0, ctxWidth + ctxPadding * 2, ctxHeight + ctxPadding * 2);
-}
-
-function drawVessel() {
-    ctx.save();
-    createVesselPath(ctx);
-    ctx.stroke();
-    ctx.restore();
-}
-
-function drawLiquid(color, height) {
-    offCtx.save();
-    offCtx.fillRect(0, ctxHeight + ctxPadding - height, ctxWidth + ctxPadding * 2, height);
-    createVesselPath(offCtx);
-    offCtx.globalCompositeOperation = 'source-in';
-    offCtx.fillStyle = color;
-    offCtx.fill();
-    offCtx.restore();
-
-    var image = new Image();
-    image.src = offCtx.canvas.toDataURL('image/png');
-    ctx.drawImage(image, 0, 0, ctxWidth, ctxHeight, 0, 0, ctxWidth, ctxHeight);
-
-    offCtx.clearRect(0, 0, ctxWidth + ctxPadding * 2, ctxHeight + ctxPadding * 2);
 }
 
 /*******************/
